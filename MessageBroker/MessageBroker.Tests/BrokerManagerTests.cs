d817353fb48bd961568eae56e1774e64bbebb5cc
Nick Stuer <nickstuer@gmail.com>
2019-11-29 22:09:59 -0600

    Add MessageBroker.Tests dotNet Core Project

---
+++ MessageBroker/MessageBroker.Tests/BrokerManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MessageBroker.Tests
+{
+    [TestFixture]
+    public class BrokerManagerTests
+    {
+        [Test]
+        public void AlwaysReturnsSameInstance()
+        {
+            var firstInstance = HDByte.MessageBroker.BrokerManager.GetMessageBroker();
+            var secondInstance = new HDByte.MessageBroker.MessageBroker();
+            var thirdInstance = HDByte.MessageBroker.BrokerManager.GetMessageBroker();
+
+            Assert.That(firstInstance, Is.SameAs(thirdInstance));
+            Assert.That(firstInstance, Is.Not.SameAs(secondInstance));
+        }
+    }
+}
