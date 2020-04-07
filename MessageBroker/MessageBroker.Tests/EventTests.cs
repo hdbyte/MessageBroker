6852bef22109b5cb1d58357def9645c0ea819474
Nick Stuer <nickstuer@gmail.com>
2020-04-06 20:37:29 -0500

    Add BaseEvent, GenericEvent and update to 1.1.1.

---
+++ MessageBroker/MessageBroker.Tests/EventTests.cs
@@ -0,0 +1,35 @@
+﻿using HDByte.MessageBroker.Events;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace MessageBroker.Tests
+{
+    [TestFixture]
+    public class EventTests
+    {
+
+        [Test]
+        public void BaseEventTimestampWorks()
+        {
+            var applicationStartTime = Process.GetCurrentProcess().StartTime.ToUniversalTime();
+            var offsetTimestamp = ((DateTime.UtcNow - applicationStartTime).TotalMilliseconds) / 1000;
+
+            var baseEvent = new BaseEvent();
+            var beforeTimestamp = Double.Parse(baseEvent.Timestamp);
+
+            Thread.Sleep(2000);
+
+            var baseEvent2 = new BaseEvent();
+            var afterTimestamp = Double.Parse(baseEvent2.Timestamp);
+            var difference = afterTimestamp - beforeTimestamp;
+
+            Assert.That(difference > 1.5 && difference < 2.5, Is.EqualTo(true));
+            Assert.That(beforeTimestamp - offsetTimestamp < .01, Is.EqualTo(true));
+        }
+
+    }
+}
---
+++ MessageBroker/MessageBroker/Events/BaseEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace HDByte.MessageBroker.Events
+{
+    public class BaseEvent
+    {
+        public static DateTime _applicationStartTime = Process.GetCurrentProcess().StartTime.ToUniversalTime();
+        public string Timestamp;
+
+        public BaseEvent()
+        {
+            decimal timeDifference = Convert.ToDecimal((DateTime.UtcNow - _applicationStartTime).TotalMilliseconds) / 1000;
+            Timestamp = timeDifference.ToString("00000.000");
+        }
+    }
+}
